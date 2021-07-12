c7ce8d1335eca5ad4f8eebaf5fbff4261bbcfd0d
thecil <53628989+thecil@users.noreply.github.com>
2021-07-11 20:05:18 -0400

    myAliens
    build/show aliens for catalogue

--- client/assets/js/components.js
+++ client/assets/js/components.js
@@ -9,33 +9,32 @@ function menu(){
 
     var menu = `<nav class="menu navbar navbar-expand-md navbar-light fixed-top bg-ligh p-4" style="opacity:0.9;">
       <div class="container">
-        <img src="assets/images/illuminati.png" class="align-self-start mr-3" style="width:40px; height:40px;>
-          <a class="navbar-brand" href="index.html"><b>CryptoAliens</b></a>
-          <button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
-              data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false"
-              aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
+        <img src="assets/images/illuminati.png"  style="width:40px; height:40px;>
+        <a class="navbar-brand " href="index.html"><b>CryptoAliens</b></a>
 
-          <div class="navbar-collapse collapse  justify-content-end" id="navbarsExampleDefault">
+          <div class="navbar-collapse collapse  justify-content-end ml-4" id="navbarsExampleDefault">
 
               <div align="right">
                   <ul class="navbar-nav mr-auto ">
-                    <img src="assets/images/vortex.png" style="width:40px; height:40px;"/>
+                    <img src="assets/images/vortex.png" class="align-self-baseline" style="width:30px; height:30px;"/>
                       <li class="nav-item">
                           <a class="nav-link" href="index.html"><b>Home</b></a>
                       </li>
-                      <img src="assets/images/blockchain.png" style="width:40px; height:40px;"/>
+                      <img src="assets/images/blockchain.png" class="align-self" style="width:30px; height:30px;"/>
                       <li class="nav-item">
                           <a class="nav-link" href="market.html"><b>Market</b></a>
                       </li>
-                        <img src="assets/images/aliens.png" style="width:40px; height:40px;"/>
+                        <img src="assets/images/aliens.png" class="align-self" style="width:30px; height:30px;"/>
                       <li class="nav-item">
                           <a class="nav-link" href="myAliens.html"><b>My Aliens</b></a>
                       </li>
-                      <img src="assets/images/dna.png" style="width:40px; height:40px;"/>
+                      <img src="assets/images/dna.png" class="align-self" style="width:30px; height:30px;"/>
                       <li class="nav-item">
                           <a class="nav-link" href="factory.html"><b>Alien-Factory</b></a>
+                      </li>
+                      <img src="assets/images/ufo.png" class="align-self" style="width:30px; height:30px;"/>
+                      <li class="nav-item">
+                          <a class="nav-link" href="clone.html"><b>Clone-Aliens</b></a>
                       </li>
 
                       <li class="nav-item">
--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 
 var web3 = new Web3(Web3.givenProvider)
-var alienFactoryInstance, alienMarketInstance
+var alienFactoryInstance, alienMarketInstance, user, contractOwner
 var alienFactory_adderss = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 var alienMarketplace_address = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
 
@@ -10,9 +10,49 @@ $(document).ready(function(){
     alienFactoryInstance = new web3.eth.Contract(abiAlienFactory, alienFactory_adderss, {from: accounts[0]})
     alienMarketInstance = new web3.eth.Contract(abiAlienMarketplace, alienMarketplace_address, {from: accounts[0]})
 
+    alienFactoryInstance.methods.owner().call().then(test => {
+      contractOwner = test;
+    });
+    user = accounts[0];
+
     console.log("alien Factory", alienFactoryInstance);
     console.log("alien Marketplace", alienMarketInstance);
 
   }).catch( error => console.error)
 
+});
+
+$('#createAlien').click(function(){
+  var _genes =  getDna()
+
+  alienFactoryInstance.methods.createAlienGen0(_genes)
+  .send({}, function(error, txHash){
+    if(error){
+      console.log(error)
+    }else{console.log(txHash)}
+  })
+
 })
+
+//Get aliens of a current address
+async function myAliens() {
+  var arrayId = await alienFactoryInstance.methods.getAllAliens(user).call();
+  for (i = 0; i < arrayId.length; i++) {
+    console.log("appending MyAliens")
+    await appendAliens(arrayId[i])
+  }
+}
+
+//Appending cats for catalog
+async function appendAliens(id) {
+  log("appendAliens")
+  var alien = await alienFactoryInstance.methods.getAlien(id).call()
+  console.log("appending", alien)
+  appendAlien(alien[0], id, alien['_generation'])
+}
+
+async function singleAlien() {
+  var id = get_variables().alienId
+  var alien = await alienFactoryInstance.methods.getAlien(id).call()
+  singleAlien(alien[0], id, alien['_generation'])
+}
